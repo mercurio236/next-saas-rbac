9f545d482c2db1408f109f3944a487e147ec749b
Arley Souto <macx15@hotmail.com>
2026-07-22 00:39:56 -0300

    feat: adição de validação de permissões e novos sujeitos com Zod

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,15 +1,29 @@
 import {
-  createMongoAbility,
   AbilityBuilder,
-  type MongoAbility,
+  createMongoAbility,
   type CreateAbility,
+  type MongoAbility,
 } from '@casl/ability'
+import z from 'zod'
 import type { User } from './models/user'
 import { permissions } from './permissions'
-import type { UserSubject } from './subjects/user'
-import type { ProjectSubject } from './subjects/project'
+import { billingSubject } from './subjects/billing'
+import { inviteSubject } from './subjects/invite'
+import { organizationSubject } from './subjects/organization'
+import { projectSubject } from './subjects/project'
+import { userSubject } from './subjects/user'
 
-type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+
+  z.tuple([z.literal('manage'), z.literal('all')]),
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
