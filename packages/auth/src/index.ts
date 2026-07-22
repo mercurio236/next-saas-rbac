2f717340f57764f0ed8214508b675aa80dab4d5f
Arley Souto <macx15@hotmail.com>
2026-07-22 02:05:43 -0300

    feat: atualização de permissões e ajustes nos sujeitos de organização

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -13,6 +13,10 @@ import { organizationSubject } from './subjects/organization'
 import { projectSubject } from './subjects/project'
 import { userSubject } from './subjects/user'
 
+export * from './models/organization'
+export * from './models/user'
+export * from './models/projects'
+
 const appAbilitiesSchema = z.union([
   projectSubject,
   userSubject,
