8ac17638a17621d4634f853b483c5f57660c4c35
Arley Souto <macx15@hotmail.com>
2026-08-01 19:35:16 -0300

    feat: adicionar autenticação com e-mail e senha e suporte ao JWT

---
+++ apps/api/src/http/routes/auth/authenticate-with-password.ts
@@ -0,0 +1,59 @@
+import { prisma } from '@/lib/prisma'
+import { compare } from 'bcryptjs'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+export async function authenticateWithPassword(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/sessions/password',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Autheticate with e-mail and password',
+        body: z.object({
+          email: z.email(),
+          password: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const { email, password } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) {
+        return reply.status(400).send({ message: 'Invalid credentials.' })
+      }
+
+      if (userFromEmail.passwordHash === null) {
+        return reply
+          .status(400)
+          .send({ message: 'User does not have a password, use social login.' })
+      }
+
+      const isPassoword = await compare(password, userFromEmail.passwordHash)
+
+      if (!isPassoword) {
+        return reply.status(400).send({ message: 'Invalid credentials.' })
+      }
+
+      const token = reply.jwtSign(
+        {
+          sub: userFromEmail.id,
+        },
+        {
+          sign: {
+            expiresIn: '7d',
+          },
+        }
+      )
+
+      return reply.status(201).send(token)
+    }
+  )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,5 +1,6 @@
 import { fastify } from 'fastify'
 import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUI from '@fastify/swagger-ui'
 
@@ -11,6 +12,7 @@ import {
 } from 'fastify-type-provider-zod'
 
 import { createAccount } from './routes/auth/create-account'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -33,9 +35,14 @@ app.register(fastifySwaggerUI, {
   routePrefix: '/docs',
 })
 
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
 app.register(fastifyCors)
 
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
