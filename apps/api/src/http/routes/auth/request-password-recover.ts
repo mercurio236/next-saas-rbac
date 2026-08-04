f0de7cf7ad149d7f5692e8465c03f6cc3785d132
Arley Souto <macx15@hotmail.com>
2026-08-04 07:36:59 -0300

    feat: adicionar rotas para recuperação e redefinição de senha

---
+++ apps/api/src/http/routes/auth/request-password-recover.ts
@@ -0,0 +1,49 @@
+import { prisma } from '@/lib/prisma'
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { BadRequestError } from '../_error/bad-request-error'
+import { auth } from '@/http/middlewares/auth'
+
+export async function requestPasswordRecovered(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/password/recover',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated user profile',
+        body: z.object({
+          email: z.email(),
+        }),
+        // response: {
+        //   201: z.null(),
+        // },
+      },
+    },
+    async (request, reply) => {
+      const { email } = request.body
+
+      const userFromEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (!userFromEmail) {
+        // We don't want people to know if user realy exists
+        return reply.status(201).send()
+      }
+
+      const { id: code } = await prisma.token.create({
+        data: {
+          type: 'PASSWORD_RECOVER',
+          userId: userFromEmail.id,
+        },
+      })
+
+      //send e-mail with password recover link
+
+      return reply.status(201).send()
+    }
+  )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -15,6 +15,8 @@ import { createAccount } from './routes/auth/create-account'
 import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 import { getProfile } from './routes/auth/get-profile'
 import { errorHandler } from './error-handler'
+import { requestPasswordRecovered } from './routes/auth/request-password-recover'
+import { resetPassword } from './routes/auth/reset-password'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -48,6 +50,8 @@ app.register(fastifyCors)
 app.register(createAccount)
 app.register(authenticateWithPassword)
 app.register(getProfile)
+app.register(requestPasswordRecovered)
+app.register(resetPassword)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running!')
