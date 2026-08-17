046ce353e759824f4ec1d034d01d8f11158ae425
Arley Souto <macx15@hotmail.com>
2026-08-16 21:00:56 -0300

    feat(auth): implementar validação de formulário de login e exibir mensagens de erro

--- apps/web/src/app/auth/sign-in/actions.ts
+++ apps/web/src/app/auth/sign-in/actions.ts
@@ -1,17 +1,46 @@
 'use server'
 
 import { signInWithPassword } from '@/src/http/sign-in-with-password'
+import { HTTPError } from 'ky'
+import { z } from 'zod'
 
-export async function singInWithEmailAndPassword(
-  previousStata: unknown,
-  data: FormData
-) {
-  const { email, password } = Object.fromEntries(data)
+const signSchema = z.object({
+  email: z.email('Please, provide a valid e-mail address.'),
+  password: z.string().min(1, 'Please, provide your password'),
+})
 
-  const result = await signInWithPassword({
-    email: String(email),
-    password: String(password),
-  })
+export async function singInWithEmailAndPassword(_: unknown, data: FormData) {
+  const result = signSchema.safeParse(Object.fromEntries(data))
 
-  console.log(result)
+  if (!result.success) {
+    const flattenError = z.flattenError(result.error)
+    const errors = flattenError.fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
 }
--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -10,22 +10,43 @@ import Image from 'next/image'
 
 import githubIcon from '@/src/assets/github.svg'
 import { useActionState } from 'react'
-import { Loader2 } from 'lucide-react'
+import { AlertTriangle, Loader2 } from 'lucide-react'
+import { Alert, AlertDescription, AlertTitle } from '@saas/ui/components/alert'
 
 export function SignForm() {
-  const [state, formAction, isPending] = useActionState(
+  const [{ success, message, errors }, formAction, isPending] = useActionState(
     singInWithEmailAndPassword,
-    null
+    { success: false, message: null, errors: null }
   )
   return (
     <form action={formAction} className="space-y-4">
+      {success === false && message && (
+        <Alert variant="destructive">
+          <AlertTriangle className="size-4" />
+          <AlertTitle>Sign in failed!</AlertTitle>
+          <AlertDescription>
+            <p>{message}</p>
+          </AlertDescription>
+        </Alert>
+      )}
       <div className="space-y-1">
         <Label htmlFor="email">E-mail</Label>
         <Input name="email" type="email" id="email" />
+        {errors?.email && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.email[0]}
+          </p>
+        )}
       </div>
       <div className="space-y-1">
         <Label htmlFor="password">Password</Label>
         <Input name="password" type="password" id="password" />
+
+        {errors?.password && (
+          <p className="text-xs font-medium text-red-500 dark:text-red-400">
+            {errors.password[0]}
+          </p>
+        )}
 
         <Link
           href="/auth/forgot-password"
